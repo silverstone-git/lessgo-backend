bb1d13a9c564219997791afbcd0921ae0672cea3
silverstone-git <silverstone965@gmail.com>
2023-06-05 13:07:10 +0530

    changed items to include latest model props

--- api/routes/itemsRouter.ts
+++ api/routes/itemsRouter.ts
@@ -7,13 +7,13 @@ router.post('/', (req: any, res: any) => {
 	//data = MongoDBClient(process.env.MONGO_URI).collection("items").all();
 	//return data;
 	res.json({"itemList": [
-		{"name": "scooter", "price": 80, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
-		{"name": "e-bike", "price": 10, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
-		{"name": "scooter", "price": 70, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
-		{"name": "submarine", "price": 30, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
-		{"name": "jet plane", "price": 800, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
-		{"name": "passenger plane", "price": 600, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
-		{"name": "truck", "price": 20, "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "scooter", "description": "", "category": "", inStock: true, "priceRs": 80, "dateAdded": new Date('August 14, 2003'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "e-bike", "description": "", "category": "", inStock: true, "priceRs": 10, "dateAdded": new Date('August 14, 2003'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "scooter", "description": "", "category": "", inStock: true, "priceRs": 70, "dateAdded": new Date('August 14, 2003 11:20:00'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "submarine", "description": "", "category": "", inStock: true, "priceRs": 30, "dateAdded": new Date('August 14, 2003'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "jet plane", "description": "", "category": "", inStock: true, "priceRs": 800, "dateAdded": new Date('August 14, 2003'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "passenger plane", "description": "", "category": "", inStock: true, "priceRs": 600, "dateAdded": new Date('August 14, 2003'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
+		{"itemName": "truck", "description": "", "category": "", inStock: true, "priceRs": 20, "dateAdded": new Date('August 14, 2003'), "image": "https://upload.wikimedia.org/wikipedia/commons/0/05/Kawasaki_ZX-RR_2007TMS.jpg"},
 	]});
 });
 
